df7d4efafa70dbe3552e1bf78dc81442f0a3b488
Matt Day <mattsday@gmail.com>
2017-07-03 19:04:39 +0100

    Fixed manifest step

--- pipeline/generate-manifest.sh
+++ pipeline/generate-manifest.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 set -xe
-echo '  path:' $APP_PACKAGE >> git-assets/manifest.yml
+cp git-assets/manifest.yml manifest-output/manifest.yml
+echo '  path:' $APP_PACKAGE >> manifest-output/manifest.yml
+cat ./manifest-output/manifest.yml
