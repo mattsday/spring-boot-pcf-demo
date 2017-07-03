cda78a737acb189a3e76e244a8f75df84b6821e7
Matt Day <mattsday@gmail.com>
2017-07-03 19:28:40 +0100

    Fixed bug I hope

--- pipeline/generate-manifest.sh
+++ pipeline/generate-manifest.sh
@@ -2,5 +2,5 @@
 
 set -xe
 cp git-assets/manifest.yml manifest-output/manifest.yml
-echo '  path:' $APP_PACKAGE >> manifest-output/manifest.yml
+echo '  path: mvn-package-output/demo-0.0.1-SNAPSHOT.jar' >> manifest-output/manifest.yml
 cat ./manifest-output/manifest.yml
