de13f4f6f9bd013a63ce1b819edfe200bd1cb134
Matt Day <mattsday@gmail.com>
2017-07-04 08:21:43 +0100

    Initial blue/green work

---
+++ pipeline/app-colour.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xe
+
+# Login to PCF
+cf api $CF_API --skip-ssl-validation
+cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
+cf apps
+
+set +e
+
+export CURR_APP_COLOR=$(cf apps | grep "$CF_APP.$CF_APP_DOMAIN" | awk '{print $1}')
+if [[ $CURR_APP_COLOR =~ .*green.* ]]
+then
+  echo "green" > ./app-colour/current-app.txt
+  echo "blue" > ./app-colour/next-deployment.txt
+else
+  echo "blue" > ./app-colour/current-app.txt
+  echo "green" > ./app-colour/next-deployment.txt
+fi
+
+set -xe
+
+echo "Current app is: $(cat ./app-colour/current-app.txt)"
+echo "New version of app will be: $(cat ./app-colour/next-deployment.txt)"
--- pipeline/generate-manifest.sh
+++ pipeline/generate-manifest.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 set -xe
-cp git-assets/manifest.yml manifest-output/manifest.yml
+sed "s/APPNAME/$CF_APP-$(cat ./app-colour/next-deployment.txt)/" git-assets/pipeline/manifest.yml > ./manifest-output/manifest.yml
 echo '  path: '$APP_PACKAGE >> ./manifest-output/manifest.yml
 cat ./manifest-output/manifest.yml
 ls
-ls ..
