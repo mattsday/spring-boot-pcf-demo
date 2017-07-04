8c2c00f99a5d23a2716c9d12dfa1f97f09529268
Matt Day <mattsday@gmail.com>
2017-07-04 10:21:20 +0100

    Fixed sed path seperator

--- pipeline/generate-manifest.sh
+++ pipeline/generate-manifest.sh
@@ -2,7 +2,7 @@
 
 set -xe
 cp ./git-assets/pipeline/manifest.yml ./manifest-output/manifest.yml
-sed -i "s/APPNAME/$CF_APP-$(cat ./app-colour/next-deployment.txt)/" ./manifest-output/manifest.yml
-sed -i "s/APPPATH/$APP_PACKAGE/" ./manifest-output/manifest.yml
+sed -i "s%APPNAME%$CF_APP-$(cat ./app-colour/next-deployment.txt)%" ./manifest-output/manifest.yml
+sed -i "s%APPPATH%$APP_PACKAGE%" ./manifest-output/manifest.yml
 cat ./manifest-output/manifest.yml
 ls
