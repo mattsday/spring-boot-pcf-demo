8cb2d4639029afadffe95025ece3d5afb8926d2f
Matt Day <mattsday@gmail.com>
2017-07-04 06:50:46 +0100

    Fixed packaging and manifest bug

--- pipeline/generate-manifest.sh
+++ pipeline/generate-manifest.sh
@@ -2,6 +2,6 @@
 
 set -xe
 cp git-assets/manifest.yml manifest-output/manifest.yml
-echo '  path: '$APP_PACKAGE/demo-0.0.1-SNAPSHOT.jar >> ./manifest-output/manifest.yml
+echo '  path: '$APP_PACKAGE >> ./manifest-output/manifest.yml
 cat ./manifest-output/manifest.yml
-ls ..
+ls
